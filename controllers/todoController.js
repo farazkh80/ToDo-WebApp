9f5cde10295d595bd79f8f10428f5bb2fa9722a8
farazkh8 <farazkhoubsirat80@gmail.com>
2020-07-30 17:12:27 -0700

    Established the database connection

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,26 @@
+
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to the database (use mangoDB Atlas website to connect)
+mongoose.connect('mongodb+srv://faraz:test@cluster0.x9xes.mongodb.net/cluster0?retryWrites=true&w=majority');
+
+
+//create a schema
+var todoSchema = new mongoose.Schema({
+  item : String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: "get food"}).save(function(err){
+  if(err) throw err;
+  console.log('item saved');
+});
+
+
+var data = [{item:'get milk'}, {item:'walk dog'}, {item:'kick some coding ass'} ];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
