afd0c609a02fba2e19d188f576e34820ea9ecd7d
farazkh8 <farazkhoubsirat80@gmail.com>
2020-08-05 17:36:15 -0700

    Added the API(Get, Post and Delete)

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -12,27 +12,44 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: "get food"}).save(function(err){
-  if(err) throw err;
-  console.log('item saved');
-});
 
 
-var data = [{item:'get milk'}, {item:'walk dog'}, {item:'kick some coding ass'} ];
+//var data = [{item:'get milk'}, {item:'walk dog'}, {item:'kick some coding ass'} ];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
-  res.render('todo');
+  //get data from mongodb and pass it to the view
+
+  //specify the model
+  Todo.find({}, function(err, data){
+    if (err) throw err;
+    res.render('todo', {todos: data});
+  });
+
 });
 
-app.post('/todo', function(req, res){
+app.post('/todo', urlencodedParser , function(req, res){
 
+
+  //get data from the view and add it to the database
+
+  var newTodo = Todo(req.body).save(function(err, data){
+    if (err) throw err;
+
+    res.json(data);
+  });
 });
 
-app.delete('/todo', function(req, res){
-
+app.delete('/todo/:item', function (req, res) {
+    //delete the requested item from mongodb
+    Todo.find({item:req.params.item.replace(/\-/g, " ")}).deleteOne(function(err,data){
+        if(err){
+            throw err;
+        }
+        res.json(data);
+    });
 });
 
 
